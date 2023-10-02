0746bf0a1f963ebe30a37f825b58995234aa806a
うるさらこぞう <135242485+Noob-9191@users.noreply.github.com>
2023-10-03 02:01:18 +0900

    修正済み
    テストテストテストテスト

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./proj/netlibx/app/view/home/top.html'],
+  theme: {
+    extend: {},
+  },
+plugins: ["tailwindcss", "autoprefixer"],
+}
